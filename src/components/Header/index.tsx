3d0ff4ee1275491355c88b34c7dd182854d78b2f
nv <naverstay.me@gmail.com>
2024-09-09 23:00:33 +0200

    template

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,9 +2,11 @@ import clsx from 'clsx'
 import Image from 'next/image'
 import Link from 'next/link'
 import Container from "@/components/Container/Container";
-import {Input} from 'antd';
+import {Button, Dropdown, Flex, Input, MenuProps} from 'antd';
 import SvgSearch from "@/assets/icons/Search";
 import classes from './style.module.css'
+import SvgChevronDown from "@/assets/icons/ChevronDown";
+import {useMemo, useState} from "react";
 
 interface MainHeaderProps {
   className?: string
@@ -14,25 +16,87 @@ interface Props extends MainHeaderProps {
   onLinkClick?: () => void
 }
 
-function isScrolledIntoView(elem: HTMLElement, out = false) {
-  return elem.offsetTop + (out ? elem.offsetHeight : 0) < window.scrollY
-}
-
-export const MainNavigation = ({className, isDark, mobile}: Props) => {
-
-  return (
-    <ul className={className}>
-
-    </ul>
-  )
-}
-
 const MainHeader = ({className}: MainHeaderProps) => {
+  const [currency, setCurrency] = useState('1')
+  const [lang, setLang] = useState('1')
+  const [menu, setMenu] = useState('1')
+
+  const currencyItems: MenuProps['items'] = [
+    {
+      label: 'RUB',
+      key: '1',
+    },
+    {
+      label: 'USD',
+      key: '2',
+    },
+  ];
+
+  const menuItems: MenuProps['items'] = [
+    {
+      label: 'Поддержка',
+      key: '1',
+    },
+    {
+      label: 'FAQ',
+      key: '2',
+    },
+  ];
+
+  const langItems: MenuProps['items'] = [
+    {
+      label: 'Русский',
+      key: '1',
+    },
+    {
+      label: 'English',
+      key: '2',
+    },
+  ];
+
+  const handleMenuClick: MenuProps['onClick'] = (e) => {
+    setMenu(e.key);
+  };
+
+  const handleLangClick: MenuProps['onClick'] = (e) => {
+    setLang(e.key);
+  };
+
+  const handleCurrencyClick: MenuProps['onClick'] = (e) => {
+    setCurrency(e.key);
+  };
+
+  const langMenuProps = {
+    items: langItems,
+    onClick: handleLangClick,
+  };
+
+  const currencyMenuProps = {
+    items: currencyItems,
+    onClick: handleCurrencyClick,
+  };
+
+  const helpMenuProps = {
+    items: menuItems,
+    onClick: handleMenuClick,
+  };
+
+  const activeMenu = useMemo(() => {
+    return menuItems?.find(f => f.key === menu)?.label
+  }, [menu])
+
+  const activeLang = useMemo(() => {
+    return langItems?.find(f => f.key === lang)?.label
+  }, [lang])
+
+  const activeCurrency = useMemo(() => {
+    return currencyItems?.find(f => f.key === currency)?.label
+  }, [currency])
 
   return (
     <header className={clsx(classes.header, className)}>
       <Container>
-        <div className="flex gap-[50px] justify-between py-3">
+        <Flex gap="50px" justify={"space-between"} className={classes.headerWrapper}>
           <Link href={'/'} className={"flex items-center cursor-pointer"}>
             <Image
               src={'/logo.svg'}
@@ -50,10 +114,34 @@ const MainHeader = ({className}: MainHeaderProps) => {
                  placeholder="Поиск игр и приложений..."
                  prefix={<span className={"text-lg text-text-icon pr-2"}><SvgSearch/></span>}/>
 
-          <nav className="flex justify-start flex-1">
-            <MainNavigation className="hidden lg:flex space-x-5"/>
+          <nav className="flex justify-end gap-1 flex-1">
+            <Dropdown menu={helpMenuProps}>
+              <Button type={"text"}>
+                <span className={classes.btnText}>{activeMenu}</span>
+                <span className={classes.btnIcon}><SvgChevronDown/></span>
+              </Button>
+            </Dropdown>
+            <Dropdown menu={langMenuProps}>
+              <Button type={"text"}>
+                <span className={classes.btnText}>{activeLang}</span>
+                <span className={classes.btnIcon}><SvgChevronDown/></span>
+              </Button>
+            </Dropdown>
+            <Dropdown menu={currencyMenuProps}>
+              <Button type={"text"}>
+                <span className={classes.btnText}>{activeCurrency}</span>
+                <span className={classes.btnIcon}><SvgChevronDown/></span>
+              </Button>
+            </Dropdown>
+
+            <Button className={"ml-1"}>
+              <span className={classes.btnText}>Вход</span>
+            </Button>
+            <Button className={"ml-1"} type="primary">
+              <span className={classes.btnText}>Регистрация</span>
+            </Button>
           </nav>
-        </div>
+        </Flex>
       </Container>
     </header>
   )
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import {ConfigProvider} from 'antd';
 import {ReactNode, useEffect} from 'react'
 import Footer from '../Footer'
 import Header from '../Header'
@@ -12,7 +13,23 @@ export default function Layout({children}: { children: ReactNode }) {
   }, [pathname])
 
   return (
-    <>
+    <ConfigProvider theme={{
+      token: {
+        // Seed Token
+        colorPrimary: '#4e75ff',
+        colorSuccess: '#1db462',
+        colorWarning: '#ff8e26',
+        colorError: '#ff5c5c',
+        colorInfo: '#f1f3f4',
+        colorTextBase: '#6e7076',
+        colorBgBase: '#f7f7f7',
+        colorLink: '#6E7076',
+        borderRadius: 6,
+
+        // Alias Token
+        colorBgContainer: '#fff',
+      },
+    }}>
       <Head>
         <title>{{title: 'page title'}}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
@@ -26,11 +43,12 @@ export default function Layout({children}: { children: ReactNode }) {
       </Head>
       <div className="min-h-[100vh] flex flex-col relative">
         <Header/>
-        <main className="min-h-full flex items-center py-6 flex flex-col smooth-scroll mx-auto grow w-[100%]" id="layout">
+        <main className="min-h-full flex items-center py-6 flex flex-col smooth-scroll mx-auto grow w-[100%]"
+              id="layout">
           {children}
         </main>
         <Footer/>
       </div>
-    </>
+    </ConfigProvider>
   )
 }
